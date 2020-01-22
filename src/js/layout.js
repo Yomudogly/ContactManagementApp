e8f8d434041abc577dadc7a340e68848475ad388
Roman Khalnepesov <khalnepesov@gmail.com>
2020-01-22 00:21:19 +0000

    props in layouts

--- src/js/layout.js
+++ src/js/layout.js
@@ -14,7 +14,7 @@ import { EditContact } from "./views/EditContact.js";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-export const Layout = () => {
+export const Layout = props => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	//const basename = process.env.BASENAME || "";
